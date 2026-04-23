655c7be585c14d33d7693097890f7f828f649745
Alberto <alberrto.ramos@gmail.com>
2026-04-22 22:20:45 -0600

    Implementacion de binding

--- app/src/main/java/com/example/finarch/ui/auth/RegistrarUsuario.kt
+++ app/src/main/java/com/example/finarch/ui/auth/RegistrarUsuario.kt
@@ -2,62 +2,63 @@ package com.example.finarch.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.finarch.R
-import com.google.android.material.textfield.TextInputLayout
-import com.example.finarch.utils.*
 import androidx.core.widget.addTextChangedListener
+import com.example.finarch.databinding.ActivityRegistrarUsuarioBinding
+import com.example.finarch.utils.CampoError
+import com.example.finarch.utils.validacionDeUsusario
 
 class RegistrarUsuario : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrarUsuarioBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registrar_usuario)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityRegistrarUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         //Redireccion a login por medio de textView
-        val tvRegistrar: TextView = findViewById(R.id.tvRegistrar)
+        val tvRegistrar = binding.tvRegistrar
         tvRegistrar.setOnClickListener {
             val intent = Intent(this@RegistrarUsuario, Login::class.java)
             startActivity(intent)
         }
 
-
-        val button: Button = findViewById(R.id.btnRegistrarse)
+        //Variables a utilizar
+        val nombre = binding.tilNombre
+        val email = binding.tilEmail
+        val contra = binding.tilPassword
+        val contraConf = binding.tilConPassword
+        val button = binding.btnRegistrarse
+        //Limpieza de mensajes de error para evitar confusion del usuario
+        nombre.editText?.addTextChangedListener {
+            nombre.error = null
+            nombre.isErrorEnabled = false
+        }
+        email.editText?.addTextChangedListener {
+            email.error = null
+            email.isErrorEnabled = false
+        }
+        contra.editText?.addTextChangedListener {
+            contra.error = null
+            contra.isErrorEnabled = false
+        }
+        contraConf.editText?.addTextChangedListener {
+            contraConf.error = null
+            contraConf.isErrorEnabled = false
+        }
         button.setOnClickListener {
             //Validacion de campos antes de registrar al usuario
-            val nombre = findViewById<TextInputLayout>(R.id.tilNombre)
-            val email = findViewById<TextInputLayout>(R.id.tilEmail)
-            val contra = findViewById<TextInputLayout>(R.id.tilPassword)
-            val contraConf = findViewById<TextInputLayout>(R.id.tilConPassword)
             val resultadoValidacion = validacionDeUsusario(nombre.editText?.text.toString().trim(), email.editText?.text.toString().trim(), contra.editText?.text.toString().trim(), contraConf.editText?.text.toString().trim())
-
-            //Limpieza de mensajes de error para evitar confusion del usuario
-            nombre.editText?.addTextChangedListener {
-                nombre.error = null
-                nombre.isErrorEnabled = false
-            }
-            email.editText?.addTextChangedListener {
-                email.error = null
-                email.isErrorEnabled = false
-            }
-            contra.editText?.addTextChangedListener {
-                contra.error = null
-                contra.isErrorEnabled = false
-            }
-            contraConf.editText?.addTextChangedListener {
-                contraConf.error = null
-                contraConf.isErrorEnabled = false
-            }
 
             if(resultadoValidacion == null){
                 //Redireccion a login por medio de boton
