bc2aeeaeeb70ecb39c26fff1053ad5d7828f3dbf
Alberto <alberrto.ramos@gmail.com>
2026-04-28 18:52:20 -0600

    Implementacion de registro de usuario

--- app/src/main/java/com/example/finarch/ui/auth/RegistrarUsuario.kt
+++ app/src/main/java/com/example/finarch/ui/auth/RegistrarUsuario.kt
@@ -2,23 +2,36 @@ package com.example.finarch.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.addTextChangedListener
 import com.example.finarch.databinding.ActivityRegistrarUsuarioBinding
+import com.example.finarch.model.Usuario
 import com.example.finarch.utils.CampoError
 import com.example.finarch.utils.validacionDeUsusario
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class RegistrarUsuario : AppCompatActivity() {
 
+    //Inicializacion de variable para usar viewbinding
     private lateinit var binding: ActivityRegistrarUsuarioBinding
+    //Inicializacion de variable para usar firebase authentification
+    private lateinit var auth: FirebaseAuth
+    companion object{
+        private const val TAG = "ResigtrarUSuario"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityRegistrarUsuarioBinding.inflate(layoutInflater)
+        auth = FirebaseAuth.getInstance()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -39,6 +52,7 @@ class RegistrarUsuario : AppCompatActivity() {
         val contra = binding.tilPassword
         val contraConf = binding.tilConPassword
         val button = binding.btnRegistrarse
+
         //Limpieza de mensajes de error para evitar confusion del usuario
         nombre.editText?.addTextChangedListener {
             nombre.error = null
@@ -56,14 +70,43 @@ class RegistrarUsuario : AppCompatActivity() {
             contraConf.error = null
             contraConf.isErrorEnabled = false
         }
+
         button.setOnClickListener {
             //Validacion de campos antes de registrar al usuario
             val resultadoValidacion = validacionDeUsusario(nombre.editText?.text.toString().trim(), email.editText?.text.toString().trim(), contra.editText?.text.toString().trim(), contraConf.editText?.text.toString().trim())
 
             if(resultadoValidacion == null){
-                //Redireccion a login por medio de boton
-                val intent = Intent(this@RegistrarUsuario, Login::class.java)
-                startActivity(intent)
+                //Inicializacion de variable para acceder a firestore
+                val db = Firebase.firestore
+
+                //Creacion de credenciales de usuario para ralizar login
+                auth.createUserWithEmailAndPassword(email.editText?.text.toString(), contra.editText?.text.toString())
+                    .addOnSuccessListener { resultado ->
+                        //Agregando data del usuario a Firestore
+                        val usuario = Usuario(
+                            nombre = nombre.editText?.text.toString(),
+                            correo =email.editText?.text.toString(),
+                            uid = resultado.user?.uid ?: ""
+                        )
+
+                        db.collection("users")
+                            .document(resultado.user?.uid!!).set(usuario)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Usuario Creado correctamente", Toast.LENGTH_SHORT).show()
+                                Log.d(TAG, "Usuario agregado correctamente")
+
+                                //Redireccion a login por medio de boton
+                                val intent = Intent(this@RegistrarUsuario, Login::class.java)
+                                startActivity(intent)
+                            }
+                            .addOnFailureListener { e ->
+                                Toast.makeText(this, "Error al crear usuario", Toast.LENGTH_SHORT).show()
+                                Log.w(TAG, "Error adding document", e)
+                            }
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
             }
             else{
                 var campo = resultadoValidacion.campo
